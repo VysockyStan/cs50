9b9460da37092513785a99192f181e2be6819a02
Stanislav Vysocky <stanown@gmail.com>
2016-09-20 12:13:41 +0300

    Update water.c

--- water.c
+++ water.c
@@ -7,21 +7,26 @@ int main()
 // according with the length of his or her shower in minutes
 
 //i didn't comment this code cause it's too evident
-    int minute = 0;
+   
     int bottle_amount = 0;
+    int minute;
 
-    do
+    do 
     {
         printf("Enter the length of your shower: \n");
-        scanf("%d", &minute);
-        if (minute > 0)
-        {
+        minute = GetInt();
+    }
+    while (minute <= 0);
+    
+    
+   if (minute > 0)
+   {
         bottle_amount = minute * 12;
         printf("Number of bottle is: %d\n", bottle_amount);
-        }
-        else printf("Retry");
-    }
-    while (minute > 0);
+   }
+        
+    else printf("Retry");
+    
 
 
 return 0;
