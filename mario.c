42e8f239af67fd8224d97410b7508892c8f864b3
Stanislav Vysocky <stanown@gmail.com>
2016-09-20 12:51:37 +0300

    Update mario.c

--- mario.c
+++ mario.c
@@ -3,24 +3,29 @@
 
 int main()
 {
-    int length;
+   int length;
 
     do
     {
        printf("Enter amount of rows in the pyramid < 24: ");
-       scanf("%i", &length);
+       length = GetInt();
     }
-    while(length < 1 || length > 23);
+    while(length < 0 || length > 23);
 
     //Creats raws
-    for(int i = 1; i < length; i++)
+    for(int i = 0; i < length; i++)
     {
         //Creates spaces
-        for(int j = 0; j < length-i-1; j++) printf(" ");
-
+        for(int j = 0; j < length-i-1; j++) 
+        {
+            printf(" ");
+        }
+        
         //Creates left side of the pyramid
-        for(int j = 0; j <= i; j++) printf("#");
-
+        for(int j = 0; j <= i; j++) 
+        {
+            printf("#");
+        }
         //Creates center of the pyramid
         printf("  ");
 
